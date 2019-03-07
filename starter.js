dbee2daffd8d0493e90f3d2030b55e5891d736b2
norahx3x <alnoor0416@gmail.com>
2019-03-07 11:34:52 +0300

    finaal

--- starter.js
+++ starter.js
@@ -22,32 +22,13 @@ $('.zone').on("click", function (event) {
 function checkAll(box) {
     var dives = $('.green');
     var boxes = $('.zone');
-<<<<<<< HEAD
-    if (dives.length == boxes.length)
-        $('.message').text("congrats ;)");
-=======
     if (dives.length == boxes.length){
         $('.message').text("congrats");
         $('.pyro').css('display','inline');
 
     }
->>>>>>> 700467134fb332e086a21a4c59750a2dc42e2dee
 
 
 }
 
-        // Select none
-//         $("A[href='#select_none']").click( function() {
-//             $("#" + $(this).attr('rel') + " INPUT[type='checkbox']").attr('checked', false);
-//             return false;
-//         });
-//         // Invert selection
-//         $("A[href='#invert_selection']").click( function() {
-//             $("#" + $(this).attr('rel') + " INPUT[type='checkbox']").each( function() {
-//                 $(this).attr('checked', !$(this).attr('checked'));
-//             });
-//             return false;
-//         });
-//     });
-// </script>
 
