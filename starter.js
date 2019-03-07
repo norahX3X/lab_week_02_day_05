cb0c937bf81bdd664ab2a2236cbb88ddfb3a9973
norahx3x <alnoor0416@gmail.com>
2019-03-07 10:55:05 +0300

    functions

--- starter.js
+++ starter.js
@@ -1,1 +1,29 @@
 console.log("starter.js is connected")
+
+
+$('.zone').on("mouseover",function( event ) {   
+    // highlight the mouseover target
+    //event.target.addClass('.green');
+    event.target.style.backgroundColor = "orange";
+});
+
+$('.zone').on("mouseout",function( event ) {   
+    // highlight the mouseover target
+    event.target.style.backgroundColor = "white";
+});
+$('.zone').on("click",function( event ) {   
+    // highlight the mouseover target
+    event.target.style.backgroundColor = "orange";
+    //event.target.removeEventListener("mouseout",handleMouseDown);
+    event.target.off("mouseout",handleMouseDown);
+
+});
+
+function checkAll (box){
+    
+}
+
+// document.getElementsByClassName("zone").addEventListener("mouseover", function( event ) {   
+//     // highlight the mouseover target
+//     event.target.style.color = "orange";
+// });
