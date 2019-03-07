5ca800d907c8a54b5ff78c19c969c7e6997f4777
YOUR GITHUB NAME <areej@Areejs-MacBook-Pro.local>
2019-03-07 11:05:17 +0300

    checkbox

--- starter.js
+++ starter.js
@@ -19,11 +19,34 @@ $('.zone').on("click",function( event ) {
 
 });
 
-function checkAll (box){
+ function checkAll (box){
+    $(document).ready( function() {
+        // Select all
+        $(".zone").click( function() {
+            $(".green" + $(checkAll).attr('rel') + " INPUT[type='checkbox']").attr('checked', true);
+            return false;
+        });
+        
+        
+        
+        // Select none
+//         $("A[href='#select_none']").click( function() {
+//             $("#" + $(this).attr('rel') + " INPUT[type='checkbox']").attr('checked', false);
+//             return false;
+//         });
+//         // Invert selection
+//         $("A[href='#invert_selection']").click( function() {
+//             $("#" + $(this).attr('rel') + " INPUT[type='checkbox']").each( function() {
+//                 $(this).attr('checked', !$(this).attr('checked'));
+//             });
+//             return false;
+//         });
+//     });
+// </script>
     
-}
+// }
 
-// document.getElementsByClassName("zone").addEventListener("mouseover", function( event ) {   
-//     // highlight the mouseover target
-//     event.target.style.color = "orange";
-// });
+// // document.getElementsByClassName("zone").addEventListener("mouseover", function( event ) {   
+// //     // highlight the mouseover target
+// //     event.target.style.color = "orange";
+// // });
